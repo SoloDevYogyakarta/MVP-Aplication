5bcf9b95ca822234ffba1b17478c8fdd0cbe683e
Kenedy - <kenedy-@plznb7220310100.supporting.corp.bankmandiri.co.id>
2023-10-21 21:36:48 +0700

    add user seeder

---
+++ src/database/seeders/20231021142838-user-seeder.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const { faker } = require('@faker-js/faker');
+const { nanoid } = require('nanoid');
+const bcrypt = require('bcrypt');
+
+const makePassword = async (password) => {
+  return await bcrypt.hashSync(password, 10);
+};
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const password = await makePassword('password');
+    return queryInterface.bulkInsert(
+      {
+        tableName: 'USER',
+        schema: 'AUTHENTICATE',
+      },
+      [
+        {
+          public_id: nanoid(),
+          username: 'admin',
+          email: 'admin@yahoo.com',
+          password,
+        },
+        {
+          public_id: nanoid(),
+          username: faker.internet.userName(),
+          email: faker.internet.email(),
+          password,
+        },
+      ],
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete(
+      {
+        tableName: 'USER',
+        schema: 'AUTHENTICATE',
+      },
+      null,
+      {},
+    );
+  },
+};
