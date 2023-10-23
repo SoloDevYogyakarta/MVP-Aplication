4e1b9f079da0a284e819b082d316a7b499012f4d
Kenedy - <kenedy-@MacBook-Pro-Kenedy.local>
2023-10-23 16:56:36 +0700

    update

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const app_module_1 = require("./app.module");
 const path_1 = require("path");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.use('/', (req, res) => {
+        return res.status(200).json('OK');
+    });
     app.use('/assets/:filePath', (req, res) => {
         return res.sendFile((0, path_1.join)(__dirname, `./assets/${req.params.filePath}`));
     });
