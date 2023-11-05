52e9f8ae1999458e9605b00a7e86ac91879f514d
Kenedy - <kenedy-@MacBook-Pro-Kenedy.local>
2023-11-05 20:51:56 +0700

    feat: 🎸 add field visit to history controller

--- src/repository/history-repository/history-repository.ts
+++ src/repository/history-repository/history-repository.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { sequelize } from '../../database/entities/entity';
 import {
   userAssociate,
   userAttribute,
@@ -12,18 +13,41 @@ export class HistoryRepository {
 
   async findAll(): Promise<UserInstance[]> {
     this.logger.log(HistoryRepository.name);
-    return await userAssociate.findAll({
+    let result = await userAssociate.findAll({
       attributes: userAttribute,
       include: userHistoryInclude,
     });
+    for (const values of result) {
+      const visit = await this.visit(values.public_id);
+      result = JSON.parse(JSON.stringify(result)).map((item: UserInstance) => {
+        if (item.public_id === values.public_id) {
+          item['visit'] = visit;
+        }
+        return item;
+      });
+    }
+    return result;
   }
 
   async findOne(public_id: string): Promise<UserInstance> {
     this.logger.log(HistoryRepository.name);
-    return await userAssociate.findOne({
+    let result = await userAssociate.findOne({
       where: { public_id },
       attributes: userAttribute,
       include: userHistoryInclude,
     });
+    result = JSON.parse(JSON.stringify(result));
+    result['visit'] = await this.visit(public_id);
+    return result;
+  }
+
+  async visit(public_id: string): Promise<number> {
+    const query = await sequelize.query(
+      `SELECT a.user_id,COUNT(*) as visit FROM 'SERVICES.ORDER' AS a WHERE a.user_id = '${public_id}' `,
+    );
+    const visit = query[0].find(
+      (item: { user_id: string; visit: number }) => item.user_id === public_id,
+    ) as { user_id: string; visit: number };
+    return visit?.visit ? visit.visit : 0;
   }
 }
