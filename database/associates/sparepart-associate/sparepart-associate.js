c5cbca4041c4a7fc9bfe0725d77dc0609b07fdd8
Kenedy - <kenedy-@plznb7445860300.supporting.corp.bankmandiri.co.id>
2023-11-07 14:46:22 +0700

    update

--- database/associates/sparepart-associate/sparepart-associate.js
+++ database/associates/sparepart-associate/sparepart-associate.js
@@ -4,13 +4,23 @@ exports.sparepartAssociate = exports.sparepartInclude = exports.sparepartAttribu
 const free_text_entity_1 = require("../../../database/entities/public/free-text-entity/free-text-entity");
 const sparepart_entity_1 = require("../../../database/entities/services/sparepart-entity/sparepart-entity");
 const sparepartAttribute = {
-    exclude: [],
-    include: [],
+    include: [
+        ['createdAt', 'created_at'],
+        ['updatedAt', 'updated_at'],
+    ],
+    exclude: ['createdAt', 'updatedAt'],
 };
 exports.sparepartAttribute = sparepartAttribute;
 const sparepartInclude = [
     {
         model: free_text_entity_1.freeTextEntity,
+        attributes: {
+            include: [
+                ['createdAt', 'created_at'],
+                ['updatedAt', 'updated_at'],
+            ],
+            exclude: ['createdAt', 'updatedAt'],
+        },
         as: 'free_text',
     },
 ];
